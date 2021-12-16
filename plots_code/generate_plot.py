40cf4579d7170b57d667c014a3badc642b8d1035
John Tran <jtran989@unm.edu>
2021-12-15 22:54:36 -0700

    removed tick marks from the plot

--- plots_code/generate_plot.py
+++ plots_code/generate_plot.py
@@ -42,10 +42,6 @@ if __name__ == "__main__":
     # plt.figure(1)
     fig, ax = plt.subplots()
 
-    # Second, remove the ticks as well.
-    # ax.tick_params(bottom=False, left=False)
-    plt.tick_params(bottom=False, left=False)
-
     # First, let's remove the top, right and left spines (figure borders)
     # which really aren't necessary for a bar chart.
     # Also, make the bottom spine gray instead of black.
@@ -55,22 +51,23 @@ if __name__ == "__main__":
     ax.spines['bottom'].set_color('#DDDDDD')
 
     # Second, remove the ticks as well.
-    ax.tick_params(bottom=False, left=False)
-    plt.tick_params(bottom=False, left=False)
+    # ax.get_minor_ticks().set_visible(False)
+    # ax.tick_params(axis="both", bottom=False, left=False)
+    plt.tick_params(axis="both", which="both", bottom=False, left=False)
 
     # Third, add a horizontal grid (but keep the vertical grid hidden).
     # Color the lines a light gray as well.
     ax.set_axisbelow(True)
-    # ax.yaxis.grid(True, color='#EEEEEE')
-    # ax.xaxis.grid(False)
+    ax.yaxis.grid(True, which="both", color='#EEEEEE', linestyle='dotted')
+    ax.xaxis.grid(False)
 
     #fig.tight_layout()
 
     # zorder before plotting...
     #ax.set_axisbelow(True)
     #plt.grid(True, axis="y", which="both", linestyle='dotted', zorder=0)
-    plt.grid(True, axis="y", color='#EEEEEE', which="both",
-             linestyle='dotted', zorder=0)
+    # plt.grid(True, axis="y", color='#EEEEEE', which="both",
+    #          linestyle='dotted')
     # check if the grid looks good...
 
     #ax = fig.add_axes([0, 0, 1, 1])
@@ -78,33 +75,30 @@ if __name__ == "__main__":
     # color='...'
     # blue - b
     plt.bar(br1, output[0], color='b', width=barWidth, label=f"Hybrid time",
-            log=1, zorder=1)
+            log=1)
 
     # changed from br3 -- stack the repartition time with new hybrid time
     # change so output[3] is on bottom -- new repartitioned hybrid time
     # red - r
     plt.bar(br2, output[3], color='r', width=barWidth, label=f"Repartitioned "
                                                             f"Hybrid time",
-            log=1, zorder=1)
+            log=1)
 
     # green - g
     plt.bar(br2, output[2], color='g', width=barWidth, label=f"Repartition "
                                                              f"time",
-            bottom=output[3], log=1, zorder=1)
+            bottom=output[3], log=1)
 
     plt.xlabel(f"Number of processes")
     plt.ylabel(f"Time (s)")
     plt.title(f"{filename_prefix} Timings Plot")
 
     offset = ((1 / 2) * (num_bars - 1)) * barWidth
-    plt.xticks([r + offset for r in range(len(processes_array))],
-               processes_array)
 
     plt.legend()
 
-    # Second, remove the ticks as well.
-    # ax.tick_params(bottom=False, left=False)
-    plt.tick_params(bottom=False, left=False)
+    plt.xticks([r + offset for r in range(len(processes_array))],
+               processes_array)
 
     plt.savefig(f"{filename_prefix}_timings_plot.png")
 
@@ -115,10 +109,6 @@ if __name__ == "__main__":
     # plt.figure(1)
     fig, ax = plt.subplots()
 
-    # Second, remove the ticks as well.
-    # ax.tick_params(bottom=False, left=False)
-    plt.tick_params(bottom=False, left=False)
-
     # First, let's remove the top, right and left spines (figure borders)
     # which really aren't necessary for a bar chart.
     # Also, make the bottom spine gray instead of black.
@@ -128,14 +118,15 @@ if __name__ == "__main__":
     ax.spines['bottom'].set_color('#DDDDDD')
 
     # Second, remove the ticks as well.
-    ax.tick_params(bottom=False, left=False)
-    plt.tick_params(bottom=False, left=False)
+    # ax.get_minor_ticks().set_visible(False)
+    # ax.tick_params(axis="both", bottom=False, left=False)
+    plt.tick_params(axis="both", which="both", bottom=False, left=False)
 
     # Third, add a horizontal grid (but keep the vertical grid hidden).
     # Color the lines a light gray as well.
     ax.set_axisbelow(True)
-    # ax.yaxis.grid(True, color='#EEEEEE')
-    # ax.xaxis.grid(False)
+    ax.yaxis.grid(True, which="both", color='#EEEEEE', linestyle='dotted')
+    ax.xaxis.grid(False)
 
     #fig.tight_layout()
 
@@ -143,32 +134,29 @@ if __name__ == "__main__":
     #ax.set_axisbelow(True)
     # plt.grid(True, axis="y", which="both", linestyle='dotted', zorder=0)
     plt.grid(True, axis="y", color='#EEEEEE', which="both",
-             linestyle='dotted', zorder=0)
+             linestyle='dotted')
     # check if the grid looks good...
 
     # ax = fig.add_axes([0, 0, 1, 1])
     plt.bar(br1, output[1], color='b', width=barWidth, label=f"Hybrid Residual",
-            log=1, zorder=1)
+            log=1)
 
     # changed from br3 -- stack the repartition time with new hybrid time
     # change so output[3] is on bottom -- new repartitioned hybrid time
     plt.bar(br2, output[4], color='r', width=barWidth, label=f"Repartitioned "
                                                              f"Hybrid Residual",
-            log=1, zorder=1)
+            log=1)
 
     plt.xlabel(f"Number of processes")
     plt.ylabel(f"Residual")
     plt.title(f"{filename_prefix} Residual Plot")
 
     offset = ((1 / 2) * (num_bars - 1)) * barWidth
-    plt.xticks([r + offset for r in range(len(processes_array))],
-               processes_array)
 
     plt.legend()
 
-    # Second, remove the ticks as well.
-    # ax.tick_params(bottom=False, left=False)
-    plt.tick_params(bottom=False, left=False)
+    plt.xticks([r + offset for r in range(len(processes_array))],
+               processes_array)
 
     plt.savefig(f"{filename_prefix}_residual_plot.png")
 
